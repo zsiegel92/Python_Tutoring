376f6025d06b5e4448e10d52f106afb11d2a232d
Zach S <zsiegel92@gmail.com>
2017-12-14 10:14:45 -0800

    templating variations

---
+++ app2.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+app = Flask(__name__,static_url_path='')
+
+@app.route("/")
+def index():
+    print("HELLO")
+    return app.send_static_file('hello.html')
+
+@app.route("/<name>")
+def say_name(name):
+    text = """<!DOCTYPE html>
+    <html >
+        <head>
+            <style>
+            body {
+            background-color: #275399;
+            font-family: 'Ubuntu', sans-serif;
+            }
+            </style>
+        </head>
+        <body>
+            Hello, %s!
+            <br>
+            <br>
+            This is cool, huh?
+        </body>
+    </html>
+    """
+    return text % name
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
--- app3.py
+++ app3.py
@@ -1,16 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__,static_url_path='')
 
 @app.route("/")
 def index():
     print("HELLO")
-    return app.send_static_file('hello.html')
+    return app.send_static_file('eitan.html')
 
 @app.route("/<name>")
 def say_name(name):
-    return "Hi, " + name
-
+    return render_template('hello3.html',name=name)
 
 if __name__=="__main__":
     app.run(debug=True)
