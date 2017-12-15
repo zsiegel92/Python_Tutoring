c6501027b92035db3fea207c78e27ad475f8540b
Zach Siegel <zsiegel92@gmail.com>
2017-12-15 23:42:22 +0000

    html

--- app_codenvy.py
+++ app_codenvy.py
@@ -20,9 +20,6 @@ def friend(name):
     return "Hi, my friend, " + name
 
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
     
-
-
