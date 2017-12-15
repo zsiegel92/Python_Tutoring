365c05c65bbdd1c8ad655d80662e668a704940d1
Zach <zsiegel92@gmail.com>
2017-12-15 22:25:05 +0000

    Changes to app_codenvy.py

---
+++ app_codenvy.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route("/")
+def index():
+    print("HELLO")
+#    return "HELLO"
+    return app.send_static_file('hello.html')
+
+
+@app.route("/sayname/<name>")
+def say_name(name):
+    return "Hi, my friend, " + name
+    
+
+@app.route("/friend/friend")
+def friend(name):
+    return "Hi, my friend, " + name
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8080)
+    
+
+
